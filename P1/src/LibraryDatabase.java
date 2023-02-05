936c3690ee27f76a88033f79064c6bc718f0fb0f
Rohan Singh <rohan.b.singh54@gmail.com>
2023-02-05 14:41:58 -0500

    Added skeletal code for Library DB

--- P1/src/LibraryDatabase.java
+++ P1/src/LibraryDatabase.java
@@ -1,3 +1,45 @@
 public class LibraryDatabase {
     
+    public LibraryDatabase(int capacity){
+
+    }
+
+    public void add(Book book){
+
+    }
+
+    public Book remove(String isbn){
+        return null;
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public Book randomSelection(){
+        return null;
+    }
+
+    public Book findByTitle(String title){
+        return null;
+    }
+
+    public Book findByISBN(String isbn){
+        return null;
+    }
+
+    public Book[] getAllByAuthor(String author){
+        return null;
+    }
+
+    public void removeDuplicates(){
+    
+    }
+
+    public Book[] toArray(){
+        return null;
+    }
+
+
+
 }
