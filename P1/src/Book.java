8bb4a1bba53dc77266810131a63f9bab0ef35a6c
Rohan Singh <rohan.b.singh54@gmail.com>
2023-02-05 14:27:26 -0500

    Added Book skeletal code

---
+++ P1/src/Book.java
@@ -0,0 +1,19 @@
+public class Book{
+
+    public Book(String title, String ISBN, String Author){
+
+    }
+
+    public String getTitle(){
+        return "";
+    }
+
+    public String getISBN(){
+        return "";
+    }
+
+    public String getAuthor(){
+        return "";
+    }
+
+}
